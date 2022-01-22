ecffe53960ce332941bd4ed85c504bf8936ad5c0
krihl4n <lkolasa1989@gmail.com>
2022-01-22 16:22:56 +0100

    refactor

--- src/app/services/game-tracker.service.ts
+++ src/app/services/game-tracker.service.ts
@@ -10,62 +10,73 @@ import { GameControlService } from './game-control.service';
 })
 export class GameTrackerService {
 
+  private unknownPiece = "X_X"
   captures: String[] = []
 
-  positions = new Map<String, String>() // todo move this to a service
-  private positionsUpdates: Subject<FieldOccupation[]> | undefined
-  private piecePositionUpdates: Subject<PiecePositionUpdate> | undefined // todo learn obout subjects and ngrx
+  positions = new Map<String, String>()
 
   constructor(private gameControlService: GameControlService) {
     this.init()
   }
 
   init(): void {
-    this.positionsUpdates = this.gameControlService.getPiecePositionsSubscription()
-    this.positionsUpdates.subscribe((piecePositions: FieldOccupation[]) => {
-      for (let i = 0; i < piecePositions.length; i++) {
-        this.positions.set(piecePositions[i].field, this.getTokenFor(piecePositions[i].piece))
-      }
-    })
-
-    this.piecePositionUpdates = this.gameControlService.getPiecePositionUpdatesSubscription();
-    this.piecePositionUpdates.subscribe((update: PiecePositionUpdate) => {
-
-      if(!update.reverted) {
-        let piece = this.positions.get(update.primaryMove.from)
-        this.positions.delete(update.primaryMove.from)
-        this.positions.set(update.primaryMove.to, piece || "X_X") // todo something better than X_X
-  
-        if(update.secondaryMove) {
-          let piece = this.positions.get(update.secondaryMove.from)
-          this.positions.delete(update.secondaryMove.from)
-          this.positions.set(update.secondaryMove.to, piece || "X_X") // todo something better than X_X
-        }
-      } else {
-        let piece = this.positions.get(update.primaryMove.to)
-        this.positions.delete(update.primaryMove.to)
-        this.positions.set(update.primaryMove.from, piece || "X_X") // todo something better than X_X
-  
-        if(update.secondaryMove) {
-          let piece = this.positions.get(update.secondaryMove.to)
-          this.positions.delete(update.secondaryMove.to)
-          this.positions.set(update.secondaryMove.from, piece || "X_X") // todo something better than X_X
-        }
-      }
-      
-      if (update.pieceCapture) {
-        if (!update.reverted) {
-          this.captures.push(this.getTokenFor(update.pieceCapture.capturedPiece))
-        } else {
-           this.captures.pop() 
-        }
-      }
-    })
+    // TODO learn about ngrx and observables and subjects and do it better
+    this.subscribeToFieldOccupationUpdates();
+    this.subscribeToPiecePositionUpdates();
   }
 
+  private subscribeToFieldOccupationUpdates() {
+    this.gameControlService.getPiecePositionsSubscription().subscribe((piecePositions: FieldOccupation[]) => {
+      for (let i = 0; i < piecePositions.length; i++) {
+        this.positions.set(piecePositions[i].field, this.getTokenFor(piecePositions[i].piece));
+      }
+    });
+  }
+
+  private subscribeToPiecePositionUpdates() {
+    this.gameControlService.getPiecePositionUpdatesSubscription().subscribe((update: PiecePositionUpdate) => {
+      if (!update.reverted) {
+        this.handleMove(update);
+      } else {
+        this.handleMoveReverted(update);
+      }
+    });
+  }
+
+  private handleMove(update: PiecePositionUpdate) {
+    let piece = this.positions.get(update.primaryMove.from);
+    this.positions.delete(update.primaryMove.from);
+    this.positions.set(update.primaryMove.to, piece || this.unknownPiece);
+
+    if (update.secondaryMove) {
+      let piece = this.positions.get(update.secondaryMove.from);
+      this.positions.delete(update.secondaryMove.from);
+      this.positions.set(update.secondaryMove.to, piece || this.unknownPiece);
+    }
+
+    if (update.pieceCapture) {
+      this.captures.push(this.getTokenFor(update.pieceCapture.capturedPiece))
+    }
+  }
+
+  private handleMoveReverted(update: PiecePositionUpdate) {
+    let piece = this.positions.get(update.primaryMove.to);
+    this.positions.delete(update.primaryMove.to);
+    this.positions.set(update.primaryMove.from, piece || this.unknownPiece);
+
+    if (update.secondaryMove) {
+      let piece = this.positions.get(update.secondaryMove.to);
+      this.positions.delete(update.secondaryMove.to);
+      this.positions.set(update.secondaryMove.from, piece || this.unknownPiece);
+    }
+
+    if (update.pieceCapture) {
+      this.captures.pop()
+    }
+  }
 
   private getTokenFor(piece: Piece): String {
-    if(piece == null) {
+    if (piece == null) {
       return "";
     }
 
