f97090c5fed50d4993b8c7627da3d7fd8d237735
krihl4n <lkolasa1989@gmail.com>
2022-03-10 20:35:09 +0100

    basic chessboard background

--- src/app/board-canvas/board-canvas.component.ts
+++ src/app/board-canvas/board-canvas.component.ts
@@ -8,19 +8,47 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 export class BoardCanvasComponent implements OnInit {
 
   @ViewChild('canvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
-  private canvasContext: CanvasRenderingContext2D;
+  private context: CanvasRenderingContext2D;
 
-  constructor() { }
+  canvasSize = 700;
+  private boardFlipped = false;
+  private boardSize = this.canvasSize
+  private fieldColorLight = "#EBD1A6";
+  private fieldColorDark = "#A27551";
 
   ngOnInit(): void {
-    this.canvasContext = this.canvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
+    this.context = this.canvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
     let framesPerSecond = 30;
     setInterval(this.drawEverything.bind(this), 1000 / framesPerSecond);
   }
 
   private drawEverything() {
-    this.canvasContext.fillStyle = 'red';
-    this.canvasContext.fillRect(0, 0, 50, 50);
+    this.drawBackground()
   }
 
+  private drawBackground() {
+    let fieldSize = this.boardSize / 8;
+
+    let currentColor = this.fieldColorLight;
+    for (let col = 0; col < 8; col++) {
+      for (let row = 0; row < 8; row++) {
+        this.fillRectangle(col * fieldSize, row * fieldSize, fieldSize, fieldSize, currentColor)
+        currentColor = this.oppositeOf(currentColor)
+      }
+      currentColor = this.oppositeOf(currentColor)
+    }
+  }
+
+  private oppositeOf(color: string): string {
+    if (color == this.fieldColorLight) {
+      return this.fieldColorDark
+    } else {
+      return this.fieldColorLight
+    }
+  }
+
+  private fillRectangle(x: number, y: number, w: number, h: number, color: string) {
+    this.context.fillStyle = color;
+    this.context.fillRect(x, y, w, h);
+  }
 }
