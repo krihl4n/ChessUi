c3e4480572c0c23187517e3b7119b371a280f56d
krihl4n <lkolasa1989@gmail.com>
2022-03-15 10:41:07 +0100

    refactor

--- src/app/board-canvas/piece-movement.ts
+++ src/app/board-canvas/piece-movement.ts
@@ -13,16 +13,19 @@ export class PieceMovement {
         this.pieceOnTheMoveStart = pieceOnTheMoveStart;
         this.pieceOnTheMoveDestination = pieceOnTheMoveDestination;
         this.pieceOnTheMoveLocation = pieceOnTheMoveStart;
+
+
+
     }
 
-    get currentLocation() : Point {
+    get currentLocation(): Point {
         return {
             x: this.pieceOnTheMoveLocation.x,
             y: this.pieceOnTheMoveLocation.y
         }
     }
 
-    get destination() : Point {
+    get destination(): Point {
         return {
             x: this.pieceOnTheMoveDestination.x,
             y: this.pieceOnTheMoveDestination.y
@@ -30,67 +33,67 @@ export class PieceMovement {
     }
 
     updatePieceOnTheMove(secondsPassed: number) {
-        if (this.pieceOnTheMove) {
-            let baseSpeed = 750;
-            let xDistance = this.pieceOnTheMoveDestination.x - this.pieceOnTheMoveStart.x;
-            let yDistance = this.pieceOnTheMoveDestination.y - this.pieceOnTheMoveStart.y;
 
-            let xyDistance = Math.sqrt(xDistance * xDistance + yDistance * yDistance);
-            let steps = xyDistance / baseSpeed;
+        let baseSpeed = 750;
+        let xDistance = this.pieceOnTheMoveDestination.x - this.pieceOnTheMoveStart.x;
+        let yDistance = this.pieceOnTheMoveDestination.y - this.pieceOnTheMoveStart.y;
 
-            let xSpeed = (xDistance / steps) * secondsPassed;
-            let ySpeed = (yDistance / steps) * secondsPassed;
+        let xyDistance = Math.sqrt(xDistance * xDistance + yDistance * yDistance);
+        let steps = xyDistance / baseSpeed;
 
-            //  console.log('xSpeed: ' + xSpeed);
-            //  console.log('ySpeed:' + ySpeed);
+        let xSpeed = (xDistance / steps) * secondsPassed;
+        let ySpeed = (yDistance / steps) * secondsPassed;
 
-            let xDistanceLeft = Math.abs(this.pieceOnTheMoveDestination.x - this.pieceOnTheMoveLocation.x)
-            if (xDistanceLeft < Math.abs(xSpeed)) {
-                if (xSpeed < 0) {
-                    xSpeed = xDistanceLeft;
-                } else {
-                    xSpeed = -xDistanceLeft
-                }
-            }
+        //  console.log('xSpeed: ' + xSpeed);
+        //  console.log('ySpeed:' + ySpeed);
 
-            let yDistanceLeft = Math.abs(this.pieceOnTheMoveDestination.y - this.pieceOnTheMoveLocation.y)
-            if (yDistanceLeft < Math.abs(ySpeed)) {
-                if (ySpeed < 0) {
-                    ySpeed = yDistanceLeft;
-                } else {
-                    ySpeed = -yDistanceLeft
-                }
-            }
-
-            let dstXAchieved = false
-            let dstYAchieved = false
-
-            if (xSpeed == 0 ||
-                xSpeed > 0 && this.pieceOnTheMoveLocation.x >= this.pieceOnTheMoveDestination.x ||
-                xSpeed < 0 && this.pieceOnTheMoveLocation.x <= this.pieceOnTheMoveDestination.x
-            ) {
-                dstXAchieved = true
-            }
-
-            if (ySpeed == 0 ||
-                ySpeed > 0 && this.pieceOnTheMoveLocation.y >= this.pieceOnTheMoveDestination.y ||
-                ySpeed < 0 && this.pieceOnTheMoveLocation.y <= this.pieceOnTheMoveDestination.y
-            ) {
-                dstYAchieved = true
-            }
-
-            if (dstXAchieved && dstYAchieved) {
-                this.destinationAchieved = true;
-                return;
-            }
-
-            if (!dstXAchieved) {
-                this.pieceOnTheMoveLocation.x = this.pieceOnTheMoveLocation.x + xSpeed;
-            }
-
-            if (!dstYAchieved) {
-                this.pieceOnTheMoveLocation.y = this.pieceOnTheMoveLocation.y + ySpeed;
+        let xDistanceLeft = Math.abs(this.pieceOnTheMoveDestination.x - this.pieceOnTheMoveLocation.x)
+        if (xDistanceLeft < Math.abs(xSpeed)) {
+            if (xSpeed < 0) {
+                xSpeed = xDistanceLeft;
+            } else {
+                xSpeed = -xDistanceLeft
             }
         }
+
+        let yDistanceLeft = Math.abs(this.pieceOnTheMoveDestination.y - this.pieceOnTheMoveLocation.y)
+        if (yDistanceLeft < Math.abs(ySpeed)) {
+            if (ySpeed < 0) {
+                ySpeed = yDistanceLeft;
+            } else {
+                ySpeed = -yDistanceLeft
+            }
+        }
+
+        let dstXAchieved = false
+        let dstYAchieved = false
+
+        if (xSpeed == 0 ||
+            xSpeed > 0 && this.pieceOnTheMoveLocation.x >= this.pieceOnTheMoveDestination.x ||
+            xSpeed < 0 && this.pieceOnTheMoveLocation.x <= this.pieceOnTheMoveDestination.x
+        ) {
+            dstXAchieved = true
+        }
+
+        if (ySpeed == 0 ||
+            ySpeed > 0 && this.pieceOnTheMoveLocation.y >= this.pieceOnTheMoveDestination.y ||
+            ySpeed < 0 && this.pieceOnTheMoveLocation.y <= this.pieceOnTheMoveDestination.y
+        ) {
+            dstYAchieved = true
+        }
+
+        if (dstXAchieved && dstYAchieved) {
+            this.destinationAchieved = true;
+            return;
+        }
+
+        if (!dstXAchieved) {
+            this.pieceOnTheMoveLocation.x = this.pieceOnTheMoveLocation.x + xSpeed;
+        }
+
+        if (!dstYAchieved) {
+            this.pieceOnTheMoveLocation.y = this.pieceOnTheMoveLocation.y + ySpeed;
+        }
+
     }
 }
