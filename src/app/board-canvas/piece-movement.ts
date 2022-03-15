90b68e07ffd16f4f547e1cbc4c9a65d09d670f6f
krihl4n <lkolasa1989@gmail.com>
2022-03-15 10:59:57 +0100

    refactor

--- src/app/board-canvas/piece-movement.ts
+++ src/app/board-canvas/piece-movement.ts
@@ -8,14 +8,28 @@ export class PieceMovement {
     private pieceOnTheMoveLocation: Point
     destinationAchieved: boolean = false;
 
+    private xBaseSpeed: number;
+    private yBaseSpeed: number;
+
     constructor(pieceOnTheMove: HTMLImageElement, pieceOnTheMoveStart: Point, pieceOnTheMoveDestination: Point) {
         this.pieceOnTheMove = pieceOnTheMove;
         this.pieceOnTheMoveStart = pieceOnTheMoveStart;
         this.pieceOnTheMoveDestination = pieceOnTheMoveDestination;
         this.pieceOnTheMoveLocation = pieceOnTheMoveStart;
 
+        this.initializeBaseSpeed();
+    }
 
+    private initializeBaseSpeed() {
+        let baseSpeed = 750;
+        let xDistance = this.pieceOnTheMoveDestination.x - this.pieceOnTheMoveStart.x;
+        let yDistance = this.pieceOnTheMoveDestination.y - this.pieceOnTheMoveStart.y;
 
+        let xyDistance = Math.sqrt(xDistance * xDistance + yDistance * yDistance);
+        let steps = xyDistance / baseSpeed;
+
+        this.xBaseSpeed = xDistance / steps;
+        this.yBaseSpeed = yDistance / steps;
     }
 
     get currentLocation(): Point {
@@ -33,37 +47,17 @@ export class PieceMovement {
     }
 
     updatePieceOnTheMove(secondsPassed: number) {
+        let xSpeed = this.xBaseSpeed * secondsPassed;
+        let ySpeed = this.yBaseSpeed * secondsPassed;
 
-        let baseSpeed = 750;
-        let xDistance = this.pieceOnTheMoveDestination.x - this.pieceOnTheMoveStart.x;
-        let yDistance = this.pieceOnTheMoveDestination.y - this.pieceOnTheMoveStart.y;
+        xSpeed = this.recalculateSpeedForLastStep(xSpeed, 
+            Math.abs(this.pieceOnTheMoveDestination.x - this.pieceOnTheMoveLocation.x));
 
-        let xyDistance = Math.sqrt(xDistance * xDistance + yDistance * yDistance);
-        let steps = xyDistance / baseSpeed;
-
-        let xSpeed = (xDistance / steps) * secondsPassed;
-        let ySpeed = (yDistance / steps) * secondsPassed;
+        ySpeed = this.recalculateSpeedForLastStep(ySpeed, 
+            Math.abs(this.pieceOnTheMoveDestination.y - this.pieceOnTheMoveLocation.y));
 
         //  console.log('xSpeed: ' + xSpeed);
         //  console.log('ySpeed:' + ySpeed);
-
-        let xDistanceLeft = Math.abs(this.pieceOnTheMoveDestination.x - this.pieceOnTheMoveLocation.x)
-        if (xDistanceLeft < Math.abs(xSpeed)) {
-            if (xSpeed < 0) {
-                xSpeed = xDistanceLeft;
-            } else {
-                xSpeed = -xDistanceLeft
-            }
-        }
-
-        let yDistanceLeft = Math.abs(this.pieceOnTheMoveDestination.y - this.pieceOnTheMoveLocation.y)
-        if (yDistanceLeft < Math.abs(ySpeed)) {
-            if (ySpeed < 0) {
-                ySpeed = yDistanceLeft;
-            } else {
-                ySpeed = -yDistanceLeft
-            }
-        }
 
         let dstXAchieved = false
         let dstYAchieved = false
@@ -95,5 +89,17 @@ export class PieceMovement {
             this.pieceOnTheMoveLocation.y = this.pieceOnTheMoveLocation.y + ySpeed;
         }
 
+    }
+
+    private recalculateSpeedForLastStep(speed: number, distanceLeft: number): number {
+        if (distanceLeft < Math.abs(speed)) {
+            if (speed < 0) {
+                return distanceLeft;
+            } else {
+                return -distanceLeft
+            }
+        } else {
+            return speed
+        }
     }
 }
