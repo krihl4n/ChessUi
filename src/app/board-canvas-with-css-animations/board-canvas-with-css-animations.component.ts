55627d4b85dade5463166bbe7215693761ff229c
krihl4n <lkolasa1989@gmail.com>
2022-05-15 15:21:30 +0200

    create elements dynamically with renderer

--- src/app/board-canvas-with-css-animations/board-canvas-with-css-animations.component.ts
+++ src/app/board-canvas-with-css-animations/board-canvas-with-css-animations.component.ts
@@ -1,18 +1,31 @@
-import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ComponentFactoryResolver, ElementRef, HostListener, OnInit, Renderer2, ViewChild, ViewContainerRef } from '@angular/core';
 import { DrawingService } from '../board-canvas/drawing.service';
 import { FieldUtilsService } from '../board-canvas/field-utils.service';
+import { ChessPieceComponent } from './chess-piece/chess-piece.component';
 
 @Component({
   selector: 'app-board-canvas-with-css-animations',
   templateUrl: './board-canvas-with-css-animations.component.html',
   styleUrls: ['./board-canvas-with-css-animations.component.css']
 })
-export class BoardCanvasWithCssAnimationsComponent implements OnInit {
+export class BoardCanvasWithCssAnimationsComponent implements OnInit, AfterViewInit {
 
-  constructor(private drawingService: DrawingService, private locationUtilsService: FieldUtilsService) { }
+  constructor(
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private drawingService: DrawingService, 
+    private locationUtilsService: FieldUtilsService,
+    private renderer: Renderer2) { }
 
-  @ViewChild('canvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
+  @ViewChild('canvas', { static: true }) 
+  canvas: ElementRef;
+
   canvasContext: CanvasRenderingContext2D;
+
+  @ViewChild('boardcontainer', { static: true }) 
+  boardContainer: ElementRef<HTMLCanvasElement>;
+
+  @ViewChild('container', { read: ViewContainerRef })
+  container: ViewContainerRef;
 
   private fieldSize: number;
   canvasSize: number;
@@ -27,15 +40,54 @@ export class BoardCanvasWithCssAnimationsComponent implements OnInit {
     window.requestAnimationFrame(this.drawEverything.bind(this));
   }
 
+  ngAfterViewInit(): void {
+    
+    // const factory = this.componentFactoryResolver.resolveComponentFactory(ChessPieceComponent)
+    // const componentRef = this.container.createComponent(factory)
+
+    //  const el = document.createElement("img");
+    //  el.setAttribute('src', 'assets/white_bishop.svg')
+    //  el.setAttribute('left', "200px")
+    //  document.getElementById("boardcontainter")?.appendChild(el)
+
+    const el = this.renderer.createElement('img')
+    this.renderer.setAttribute(el, 'id', 'white_bishop');
+    this.renderer.setAttribute(el, 'src', 'assets/white_bishop.svg');
+    this.renderer.setStyle(el, 'left', this.leftPos)
+    this.renderer.listen(el, 'click', (evt) => {
+      console.log("image clicked")
+      this.onImageClick()
+      this.renderer.setStyle(el, 'left', this.leftPos)
+    })
+    
+    this.renderer.appendChild(this.boardContainer.nativeElement, el)
+    const el1 = this.renderer.createElement('img')
+    this.renderer.setAttribute(el1, 'id', 'black_bishop');
+    this.renderer.setAttribute(el1, 'src', 'assets/black_bishop.svg');
+    this.renderer.setStyle(el1, 'left', this.leftPos1)
+    this.renderer.listen(el1, 'click', (evt) => {
+      console.log("image clicked")
+      this.onImageClick1()
+      this.renderer.setStyle(el1, 'left', this.leftPos1)
+    })
+
+    this.renderer.appendChild(this.boardContainer.nativeElement, el1)
+    this.renderer.listen("document", 'animationend', () => {
+      console.log("animation")
+    })
+    this.renderer.listen(el1, 'mousedown', () => {
+      console.log("mousedown")
+    })
+    this.renderer.selectRootElement
+  }
+
   @HostListener('window:resize', ['$event'])
   onResize() {
     this.setupBoardSize(window.outerHeight)
   }
 
-
   leftPos="100px"
-
-
+  leftPos1="200px"
   onImageClick() {
     console.log("click")
     if(this.leftPos === "100px") {
@@ -44,8 +96,16 @@ export class BoardCanvasWithCssAnimationsComponent implements OnInit {
       this.leftPos = "100px"
     }
   }
-  
 
+  onImageClick1() {
+    console.log("click1")
+    if(this.leftPos1 === "200px") {
+      this.leftPos1 = "300px"; 
+    } else {
+      this.leftPos1 = "200px"
+    }
+  }
+  
   private setupBoardSize(windowHeight: number) {
     this.fieldSize = (windowHeight-200)/8
     this.canvasSize = this.fieldSize*8  
