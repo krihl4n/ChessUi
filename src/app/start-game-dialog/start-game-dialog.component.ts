5f0fa0c29f580317d9b4ae204d224fe49a671fe6
krihl4n <lkolasa1989@gmail.com>
2022-11-23 23:40:50 +0100

    selecting buttons

--- src/app/start-game-dialog/start-game-dialog.component.ts
+++ src/app/start-game-dialog/start-game-dialog.component.ts
@@ -9,7 +9,40 @@ export class StartGameDialogComponent implements OnInit {
 
   constructor() { }
 
+  isWhiteColorSelected = false
+  isRandomColorSelected = false
+  isBlackColorSelected = false
+  isComputerSelected = false
+  isFriendSelected = false
+
   ngOnInit(): void {
   }
 
+  whiteColorSelected() {
+    this.isWhiteColorSelected = true
+    this.isBlackColorSelected = false
+    this.isRandomColorSelected = false
+  }
+  
+  randomColorSelected() {
+    this.isRandomColorSelected = true
+    this.isWhiteColorSelected = false
+    this.isBlackColorSelected = false
+  }
+  
+  blackColorSelected() {
+    this.isBlackColorSelected = true
+    this.isWhiteColorSelected = false
+    this.isRandomColorSelected = false
+  }
+  
+  computerSelected() {
+    this.isComputerSelected = true
+    this.isFriendSelected = false
+  }
+  
+  friendSelected() {
+    this.isFriendSelected = true
+    this.isComputerSelected = false
+  }
 }
