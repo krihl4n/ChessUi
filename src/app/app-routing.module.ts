0de8bab64c67c528549f90eeea091602c657c47a
krihl4n <krihl4n@gmail.com>
2023-01-11 16:13:29 +0100

    remove old components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { Board2Component } from "./board2/board2.component";
-import { GameComponent } from "./game/game.component";
 import { Game2Component } from "./game2/game2.component";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/game2', pathMatch: 'full'},
-    { path: 'game', component: GameComponent},
     { path: 'game2', component: Game2Component},
     { path: 'board2', component: Board2Component },
 ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,10 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 
 import { AppComponent } from './app.component';
-import { BoardComponent } from './board/board.component';
 import { ControlsComponent } from './controls/controls.component';
 import { CapturesComponent } from './captures/captures.component';
 import { GameFinishedDialogComponent } from './game-finished-dialog/game-finished-dialog.component';
 import { AppRoutingModule } from './app-routing.module';
-import { GameComponent } from './game/game.component';
 import { Board2Component } from './board2/board2.component';
 import { StartGameDialogComponent } from './start-game-dialog/start-game-dialog.component';
 import { Game2Component } from './game2/game2.component';
@@ -18,11 +16,9 @@ import { Game2Component } from './game2/game2.component';
 @NgModule({
   declarations: [
     AppComponent,
-    BoardComponent,
     ControlsComponent,
     CapturesComponent,
     GameFinishedDialogComponent,
-    GameComponent,
     Board2Component,
     StartGameDialogComponent,
     Game2Component
