3a188d76449355db55f82f86168678a10afd6271
krihl4n <krihl4n@gmail.com>
2023-01-28 22:56:06 +0100

    delete pawn from board when en passant attack occurs

--- src/app/board/tools/piece-move-handler.ts
+++ src/app/board/tools/piece-move-handler.ts
@@ -6,18 +6,27 @@ import { PiecePositionUpdate } from "src/app/model/piece-position-update.model";
 export class PieceMoveHandler {
 
     constructor(private piecesLocations: PiecesLocations, private htmlPieceRenderer: HtmlPieceReneder, private gameService: GameService) {
+        // TODO undo attacks
         this.gameService.piecePositionChange.subscribe((update: PiecePositionUpdate) => {
-            if(update.reverted) {
+            if (update.reverted) {
                 this.movePiece(update.primaryMove.to, update.primaryMove.from)
-            }else {
+            } else {
                 this.movePiece(update.primaryMove.from, update.primaryMove.to)
             }
-            
-            if(update.secondaryMove) {
-                if(update.reverted) {
+
+            if (update.secondaryMove) { // castling
+                if (update.reverted) {
                     this.movePiece(update.secondaryMove.to, update.secondaryMove.from)
                 } else {
                     this.movePiece(update.secondaryMove.from, update.secondaryMove.to)
+                }
+            }
+
+            if (update.pieceCapture && update.primaryMove.to != update.pieceCapture.field) { // en passant
+                const pieceAtDst = this.piecesLocations.get(update.pieceCapture.field)
+                if (pieceAtDst) {
+                    this.piecesLocations.delete(update.pieceCapture.field)
+                    this.htmlPieceRenderer.deletePiece(pieceAtDst, update.pieceCapture.field)
                 }
             }
         })
@@ -25,14 +34,14 @@ export class PieceMoveHandler {
 
     movePiece(from: string, to: string) {
         const piece = this.piecesLocations.get(from)
-        if(!piece) {
+        if (!piece) {
             console.log("no piece at " + from)
             return
         }
 
         const pieceAtDst = this.piecesLocations.get(to)
         this.htmlPieceRenderer.renderPieceMovement(to, piece)
-        if(pieceAtDst) {
+        if (pieceAtDst) {
             this.htmlPieceRenderer.deletePiece(pieceAtDst, to)
         }
         this.piecesLocations.delete(from)
