de67ac68366426402de6a45ebf5c8afc697f256a
krihl4n <krihl4n@gmail.com>
2023-09-30 14:02:53 +0200

    cleanup;

--- src/app/game/captures/capture/capture.component.ts
+++ src/app/game/captures/capture/capture.component.ts
@@ -11,22 +11,21 @@ export class CaptureComponent implements OnInit {
   @Input() type: string
 
   path: string
-  width: string = "15"
-  constructor() { }
-
+  width: string
+  
   ngOnInit(): void {
     this.path = "assets/" + this.color + "_" + this.type + ".png"
     this.getWidth()
   }
 
   getWidth() {
-    if(this.type == "queen") {
+    if (this.type == "queen") {
       this.width = "25"
-    } else if(this.type == "pawn") {
+    } else if (this.type == "pawn") {
       this.width = "18"
     } else if (this.type == "knight") {
       this.width = "22"
-    }else {
+    } else {
       this.width = "20"
     }
   }
